65272322664567de40cbd37ca23352c2f5b9277f
Tom Kubasik <tom.kubasik@lunabee.com>
2021-08-05 15:25:33 +0200

    start design jam fragment

--- app/src/main/java/com/entreprisecorp/jammaster/fragments/LetsJamFragment.kt
+++ app/src/main/java/com/entreprisecorp/jammaster/fragments/LetsJamFragment.kt
@@ -4,22 +4,32 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
 import androidx.core.content.res.use
+import androidx.fragment.app.Fragment
 import com.entreprisecorp.jammaster.R
+import com.entreprisecorp.jammaster.databinding.FragmentJamBinding
 import com.entreprisecorp.jammaster.fastitems.KeyItem
 import com.google.android.material.transition.MaterialContainerTransform
 import com.mikepenz.fastadapter.GenericItem
 
-class LetsJamFragment : RecyclerFragment() {
+class LetsJamFragment : Fragment(R.layout.fragment_jam) {
+
+    private lateinit var binding: FragmentJamBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentJamBinding.inflate(inflater)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.recyclerView.transitionName = getString(R.string.email_card_detail_transition_name)
-        supportActionBar?.hide()
+        binding.root.transitionName = getString(R.string.email_card_detail_transition_name)
         sharedElementEnterTransition = MaterialContainerTransform().apply {
             // Scope the transition to a view in the hierarchy so we know it will be added under
             // the bottom app bar but over the elevation scale of the exiting HomeFragment.
@@ -29,13 +39,8 @@ class LetsJamFragment : RecyclerFragment() {
             setAllContainerColors(requireContext().themeColor(R.attr.colorSurface))
         }
     }
-
-    override fun refreshScreen() {
-        val items = arrayListOf<GenericItem>()
-        items += KeyItem("A#")
-        fillAdapter(items)
-    }
 }
+
 
 @ColorInt
 @SuppressLint("Recycle")
